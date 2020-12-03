9f0203d05046f18cab670e521fd1680bc91eb43e
Sam Warrick <warrick21@up.edu>
2020-12-02 19:44:40 -0800

    comments

--- app/src/main/java/edu/up/cs301/ExplodingKittens/EKLocalGame.java
+++ app/src/main/java/edu/up/cs301/ExplodingKittens/EKLocalGame.java
@@ -161,27 +161,14 @@ public class EKLocalGame extends LocalGame{
 
     }//makeMove
 
-
-
-    /****************************************************************************
-     * Card Methods shown below are:
-     * Attack
-     * Nope
-     * Favor
-     * SeeTheFuture
-     * Shuffle*
-     * Skip*
-     * Defuse*
-     * drawCard*
-     * Trade2
-     * Trade3
-     * Trade5
-     * nextTurn
-     * checkCard
-     ***************************************************************************/
-    //Attack card
-    //current player ends their turn without drawing a card and forces the
-    //next player to draw two cards before ending their turn
+    /**
+     * Attack card, current player ends their turn without drawing a card and
+     * forces the next player to draw two cards before ending their turn
+     * @param p
+     *      takes in a GamePlayer object parameter
+     * @return
+     *      returns a boolean if the card was found and the attack executed
+     */
     public boolean Attack(GamePlayer p) {
         int card = checkHand(currState.getCurrentPlayerHand(), 6);
         //move the card into the discard pile
@@ -210,8 +197,14 @@ public class EKLocalGame extends LocalGame{
         return true;
     }//Attack()
 
-    //Nope Card
-    //Cancels a previous move if it was an attack, skip, or nope
+    /**
+     * Nope card cancels the previous player's card if it was an attack,
+     * skip, or nope card
+     * @param p
+     *      takes in a GamePlayer object
+     * @return
+     *      returns true if the action was successful and false if not
+     */
     public boolean Nope(GamePlayer p) {
         //Get the last action played
         int actionTracker = currState.getActionsPerformed().size() - 1;
@@ -292,7 +285,11 @@ public class EKLocalGame extends LocalGame{
         }
     }
 
-    //returns the value of the last player who made a move
+    /**
+     * method to find whose turn it was previously
+     * @return
+     *      returns an int representing whose turn it was previously
+     */
     public int lastTurn(){
         int last = currState.getWhoseTurn();
         if(last == 0){
@@ -314,7 +311,10 @@ public class EKLocalGame extends LocalGame{
         return last;
     }//lastTurn()
 
-    //decrements turn
+    /**
+     * method to decrement the turn, making sure to wrap around to the next
+     * player if the number would be less than 0
+     */
     public void decrementTurn(){
         if(currState.getWhoseTurn() == 0){
             currState.setWhoseTurn(currState.getNumPlayers() - 1);
@@ -338,7 +338,9 @@ public class EKLocalGame extends LocalGame{
         }
     }//decrementTurn()
 
-    //increments turn but doesn't set cards to draw to 1
+    /**
+     * increments to the next player's turn
+     */
     public void incrementTurn(){
         //sets whose turn to next turn
         currState.setWhoseTurn((currState.getWhoseTurn()+1)%(currState.getNumPlayers()));
@@ -350,9 +352,18 @@ public class EKLocalGame extends LocalGame{
     }
 
 
-    //Favor card
-    //current player selects a target player and target player gives current
-    //player a card (randomly))
+    /**
+     * Favor card, current player selects a target player and takes a random
+     * card from their hand
+     * @param p
+     *      GamePlayer object for the current player
+     * @param target
+     *      integer for the target player of the favor card
+     * @param targCardPos
+     *      position of the target card to steal
+     * @return
+     *      returns true if the action was successful, else false
+     */
     public boolean Favor(GamePlayer p, int target, int targCardPos) {
         int card = checkHand(currState.getCurrentPlayerHand(), 8);
         if(card == -1){
@@ -380,8 +391,14 @@ public class EKLocalGame extends LocalGame{
         return true;
     }//Favor()
 
-    //See the Future card
-    //current player looks at the top three cards of the deck
+    /**
+     * See the future card, current player looks at the top three cards of
+     * the deck
+     * @param p
+     *      GamePlayer object for the current player
+     * @return
+     *      returns true if the action was successful and false otherwise
+     */
     public boolean SeeTheFuture(GamePlayer p) {
         int card = checkHand(currState.getCurrentPlayerHand(), 10);
         if(card == -1){
@@ -403,8 +420,13 @@ public class EKLocalGame extends LocalGame{
         return true;
     }//SeeTheFuture()
 
-    //Shuffle card
-    //shuffles the deck randomly
+    /**
+     * shuffle card, shuffle the deck randomly
+     * @param p
+     *      GamePlayer object for the current player
+     * @return
+     *      returns true if the action was successful and false otherwise
+     */
     public boolean Shuffle(GamePlayer p) {
         /**
          * External Citation
@@ -441,8 +463,13 @@ public class EKLocalGame extends LocalGame{
         return true;
     }
 
-    //Skip card
-    //current players turn ends without drawing a card
+    /**
+     * skip card, current player ends their turn without drawing a card
+     * @param p
+     *      GamePlayer object for the current player
+     * @return
+     *      returns true if the action was successful, false otherwise
+     */
     public boolean Skip(GamePlayer p) {
         int card = checkHand(currState.getCurrentPlayerHand(), 9);
         if(card == -1){
@@ -472,10 +499,15 @@ public class EKLocalGame extends LocalGame{
         return true;
     }
 
-    ///Play Defuse card
-    //if the current player does not have a defuse card, they lose the game,
-    //if they do have a defuse card play the defuse card and reshuffle the
-    //exploding kitten card back into the deck
+    /**
+     * Defuse card, defuses an exploding kitten card and saves the player
+     * from losing the game. If the payer does not have a defuse card they
+     * lose the game
+     * @param p
+     *      GamePlayer parameter for current player
+     * @return
+     *      returns true if the action was successful and false otherwise
+     */
     public boolean Defuse(GamePlayer player) {
         //Initalizing logMesage
         String logMessage;
@@ -516,9 +548,16 @@ public class EKLocalGame extends LocalGame{
         return false;
     }
 
-    //draw a card and end the turn of the player
-    //replaces their hand with a single exploding kitten card
-    //if they are not able to defuse an exploding kitten
+    /**
+     * draw a card action, draws a card and places it in the player's hand
+     * and ends the player's turn. If the card is an exploding kitten and
+     * they are not able to defuse the exploding kitten, populate the
+     * player's hand with 1 exploding kitten card
+     * @param player
+     *      GamePlayer object for current player
+     * @return
+     *      returns true if the action was successful and false otherwise
+     */
     public boolean drawCard(GamePlayer player) {
         //checks if deck is empty
         if(currState.getDeck().size() == 0){
@@ -567,6 +606,21 @@ public class EKLocalGame extends LocalGame{
         return true;
     }
 
+    /**
+     * trade 2 action, takes 2 cards from the player's hand and check if they
+     * are the same. If they are the same take a random card from the target
+     * player's hand.
+     * @param play
+     *      GamePlayer object for current player
+     * @param targ
+     *      int to represent the target player for the trade
+     * @param a
+     *      int for the card position of the first card to trade
+     * @param b
+     *      int for the card position of the second card to trade
+     * @return
+     *      returns true if the action was successful and false otherwise
+     */
     public boolean trade2(GamePlayer play, int targ, int a, int b) {
         if(a >= currState.getCurrentPlayerHand().size() || b >= currState.getCurrentPlayerHand().size()){
             return false;
@@ -608,6 +662,26 @@ public class EKLocalGame extends LocalGame{
         return false;
     }
 
+    /**
+     * trade 3 action, takes 3 cards and checks if they are all the same type
+     * . If they are all the same, checks the target player's hand for a card
+     * of the specified type. If a card is found move it to the current
+     * player's hand and take it out of the target player's hand.
+     * @param play
+     *      GamePlayer object for current player
+     * @param targ
+     *      int to represent the targ player
+     * @param a
+     *      int for the location of the current player hand card 1
+     * @param b
+     *      int for the location of the current player hand card 2
+     * @param c
+     *      int for the location of the current player hand card 3
+     * @param targCard
+     *      int for the card type of the desired card
+     * @return
+     *      return true for a successful action and false otherwise
+     */
     public boolean trade3(GamePlayer play, int targ, int a, int b, int c,
                           int targCard) {
         if(a >= currState.getCurrentPlayerHand().size() || b >= currState.getCurrentPlayerHand().size() || c >= currState.getCurrentPlayerHand().size()){
@@ -652,9 +726,27 @@ public class EKLocalGame extends LocalGame{
         return false;
     }
 
-    //Trade 5 cards
-    //current player selects 5 different cards and trades them for a card
-    //from the discard pile
+    /**
+     * trade 5 cards, check the 5 cards to see if they are all unique. If
+     * they are remove the target card from the discard pile and put it in
+     * the player's hand
+     * @param p
+     *      GamePlayer object for the current player
+     * @param cardPos1
+     *      int for the first card in the player's hand to trade
+     * @param cardPos2
+     *      int for the second card in the player's hand to trade
+     * @param cardPos3
+     *      int for the third card in the player's hand to trade
+     * @param cardPos4
+     *      int for the fourth card in the player's hand to trade
+     * @param cardPos5
+     *      int for the fifth card in the player's hand to trade
+     * @param target
+     *      int for the position of the target card in the discard pile
+     * @return
+     *      returns true if the action was successful and false otherwise
+     */
     public boolean trade5(GamePlayer p, int cardPos1, int cardPos2, int cardPos3,
                           int cardPos4, int cardPos5, int target) {
         if(cardPos1 >= currState.getCurrentPlayerHand().size() ||
@@ -720,9 +812,14 @@ public class EKLocalGame extends LocalGame{
     }//nextTurn
 
 
-    //check to see if the card type exists in the players hand, if it
-    // does, return the position of the card.
-    //If it doesn't return -1
+    /**
+     * method to check the player's hand for a card of the given card type
+     * @param hand
+     *      ArrayList of cards that is the player's hand
+     * @param cardTypeValue
+     *      int for the card type that is being searched for
+     * @return
+     */
     public int checkHand(ArrayList<Card> hand, int cardTypeValue) {
 
         for (int i = 0; i < hand.size(); i++) {
@@ -735,6 +832,11 @@ public class EKLocalGame extends LocalGame{
 
     //Smart defuse method allows the player to insert the exploding kitten wherever they choose
     //https://www.codelocker.net/p/101/android-create-a-popup-window-with-buttons/
+
+    /**
+     * Smart defuse method allows the player to insert the exploding kitten wherever they choose
+     * @param player
+     */
     protected void humanDefuse(final EKHumanPlayer player){
 
         /**
@@ -815,12 +917,20 @@ public class EKLocalGame extends LocalGame{
 
     }
 
-    //Getter method to return local game's instance of EKGameState
+    /**
+     * Getter method to return local game's instance of EKGameState
+     * @return
+     *      returns the current GameState
+     */
     public EKGameState getCurrState(){
         return this.currState;
     }
 
-    //Getter to get array of players
+    /**
+     * Getter to get the array of players
+     * @return
+     *  returns an array of players
+     */
     public GamePlayer[] getPlayers(){
         return players;
     }
